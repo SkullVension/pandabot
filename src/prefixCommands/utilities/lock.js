2f0bb86cf5995e3889feda1b5cede247f64fbfb2
Caleb Ephrem <quantumpandaprog@gmail.com>
2026-04-13 01:42:32 +0300

    feat: add user-specific lock and unlock functionality

--- src/prefixCommands/utilities/lock.js
+++ src/prefixCommands/utilities/lock.js
@@ -6,6 +6,16 @@ module.exports = {
   permissionsRequired: [PermissionFlagsBits.Administrator],
   async callback(client, message, args) {
     try {
+      if (args.length > 0) {
+        const user = message.mentions.users.first();
+        if (user) {
+          await message.channel.permissionOverwrites.edit(user.id, {
+            SendMessages: false,
+          });
+          await message.reply(`Channel has been locked for ${user.tag}.`);
+          return;
+        }
+      }
       await message.channel.permissionOverwrites.edit(
         message.guild.roles.everyone,
         {
