b236f7817d0489c75f49197925828c97736fb4f4
Caleb <quantumpandaprog@gmail.com>
2026-07-16 15:52:28 +0300

    feat(setStatus): update presence every hour

--- src/events/clientReady/setStatus.js
+++ src/events/clientReady/setStatus.js
@@ -1,13 +1,27 @@
 import { ActivityType } from "discord.js";
 
+let presenceInterval = null;
+
 export default (client) => {
-  client.user.setPresence({
-    activities: [
-      {
-        name: "🎍 eating some bamboo",
-        type: ActivityType.Playing,
-      },
-    ],
-    status: "online",
-  });
+  const setPresence = () => {
+    if (!client.user) return;
+
+    client.user.setPresence({
+      activities: [
+        {
+          name: "🐼 too lazy to load commands...",
+          type: ActivityType.Listening,
+        },
+      ],
+      status: "online",
+    });
+  };
+
+  setPresence();
+
+  if (presenceInterval) {
+    clearInterval(presenceInterval);
+  }
+
+  presenceInterval = setInterval(setPresence, 60 * 60 * 1000);
 };
